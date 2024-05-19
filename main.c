f852210ef6b7affbf5e1097ce2884e042221976a
Andrew Chau <andrew.chau@sjsu.edu>
2024-05-19 15:02:01 -0700

    playChord can play any amount of notes

--- main.c
+++ main.c
@@ -11,6 +11,15 @@
 //Address for LCD
 #define LCD_ADDRESS 0x27
 
+// max notes a person can play
+#define MAX_NOTES 10
+
+// tracking notes being played
+uint16_t currentFreq[MAX_NOTES] = {0};
+uint8_t currentVolume[MAX_NOTES] = {0};
+uint8_t notesPlayed = 0;
+
+uint16_t i = 0; 
 
 typedef struct note_t {
     unsigned int frequency;
@@ -36,7 +45,9 @@ Note B = {493, GPIO_PORT_P5, GPIO_PIN2, "B"};
 void initPWM(void);
 void playTone(uint16_t frequency, uint8_t volume);
 void stopTone(void);
-void playChord(uint16_t freq1, uint16_t freq2, uint8_t volume);
+void playChord(void);
+void addChordNote(uint16_t frequency, uint8_t volume);
+void removeChordNote(uint16_t frequency);
 
 
 int main(void) {
@@ -67,7 +78,15 @@ int main(void) {
 
     // Play a chord
     printf("Play chord\n");
-    playChord(A.frequency, 523, 50); // A4 and C5 notes at 50% volume
+    addChordNote(A.frequency, 523, 50); // A4 note at 50% volume
+    addChordNote(523, 50); // C5 note at 50% volume
+    addChordNote(587, 50); // D5 note at 50% volume
+    __delay_cycles(3000000); // Delay for a while
+    stopTone();
+
+    // Remove a note chord
+    printf("Remove note chord\n");
+    removeChordNote(A.frequency); // removing A4 note
     __delay_cycles(3000000); // Delay for a while
     stopTone();
 
@@ -115,127 +134,54 @@ void stopTone() {
     Timer_A_setCompareValue(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_4, 0);
 }
 
-void playChord(uint16_t freq1, uint16_t freq2, uint8_t volume) {
-    uint16_t period1 = 3000000 / freq1;
-    uint16_t period2 = 3000000 / freq2;
+void playChord() {
+    if(notesPlayed == 0) {
+        return;
+    }
 
-    uint16_t dutyCycle1 = (period1 * volume) / 100;
-    uint16_t dutyCycle2 = (period2 * volume) / 100;
+    uint32_t totalPeriod = 0;
+    uint32_t totalDutyCycle = 0;
+
+    for(i = 0; i < notesPlayed; i++) {
+        uint16_t period = 3000000 / currentFreq[i];
+        uint16_t dutyCycle = (period * currentVolume[i]) / 100;
+
+        totalPeriod += period;
+        totalDutyCycle += dutyCycle;
+    }
 
     // Software mixing example (basic)
-    uint16_t mixedPeriod = (period1 + period2) / 2;
-    uint16_t mixedDutyCycle = (dutyCycle1 + dutyCycle2) / 2;
+    uint16_t mixedPeriod = totalPeriod / notesPlayed;
+    uint16_t mixedDutyCycle = totalDutyCycle / notesPlayed;
 
     Timer_A_setCompareValue(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_0, mixedPeriod - 1);
     Timer_A_setCompareValue(TIMER_A0_BASE, TIMER_A_CAPTURECOMPARE_REGISTER_4, mixedDutyCycle);
 }
-/*************** Old code, didn't want to delete yet *************/
-//
-//
-//
-//// tone functions
-//void tone(uint32_t frequency);
-//void noTone(void);
-//
-//void main(void) {
-//    /* Stop Watchdog */
-//    MAP_WDT_A_holdTimer();
-//
-//    // initialize GPIO
-//    MAP_GPIO_setAsOutputPin(LED_PIN);
-//    MAP_GPIO_setOutputLowOnPin(LED_PIN);
-//
-//
-//    // initialize GPIO input pins
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(C.port, C.pin); // C pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(C_SHARP.port, C_SHARP.pin); // C_SHARP pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(D.port, D.pin); // D pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(E_FLAT.port, E_FLAT.pin); // E_FLAT pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(E.port, E.pin); // E pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(F.port, F.pin); // F pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(F_SHARP.port, F_SHARP.pin); // F_SHARP pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(G.port, G.pin); // G pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(G_SHARP.port, G_SHARP.pin); //G_SHARP pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(A.port, A.pin); // A pin
-//    MAP_GPIO_setAsInputPinWithPullUpResistor(B.port, B.pin); // B pin
-//
-//    while (1)
-//    {
-//        // T_C pin
-//        if (!MAP_GPIO_getInputPinValue(GPIO_PORT_P4, GPIO_PIN0))
-//        {
-//            tone(T_C);
-//            MAP_GPIO_setOutputHighOnPin(LED_PIN);
-//        }
-//        else if (!MAP_GPIO_getInputPinValue(GPIO_PORT_P4, GPIO_PIN1))
-//        {
-//            // T_D pin
-//            tone(T_D);
-//            MAP_GPIO_setOutputHighOnPin(LED_PIN);
-//        }
-//        else if (!MAP_GPIO_getInputPinValue(GPIO_PORT_P4, GPIO_PIN2))
-//        {
-//            // T_E pin
-//            tone(T_E);
-//            MAP_GPIO_setOutputHighOnPin(LED_PIN);
-//        }
-//        else if (!MAP_GPIO_getInputPinValue(GPIO_PORT_P4, GPIO_PIN3))
-//        {
-//            // T_F pin
-//            tone(T_F);
-//            MAP_GPIO_setOutputHighOnPin(LED_PIN);
-//        }
-//        else if (!MAP_GPIO_getInputPinValue(GPIO_PORT_P4, GPIO_PIN4))
-//        {
-//            // T_G pin
-//            tone(T_G);
-//            MAP_GPIO_setOutputHighOnPin(LED_PIN);
-//        }
-//        else if (!MAP_GPIO_getInputPinValue(GPIO_PORT_P4, GPIO_PIN5))
-//        {
-//            // T_A pin
-//            tone(T_A);
-//            MAP_GPIO_setOutputHighOnPin(LED_PIN);
-//        }
-//        else if (!MAP_GPIO_getInputPinValue(GPIO_PORT_P4, GPIO_PIN6))
-//        {
-//            // T_B pin
-//            tone(T_B);
-//            MAP_GPIO_setOutputHighOnPin(LED_PIN);
-//        }
-//        else
-//        {
-//            // return
-//            noTone();
-//            MAP_GPIO_setOutputLowOnPin(LED_PIN);
-//        }
-//    }
-//}
-//
-//// Function to generate a tone of a given frequency (in Hz)
-//void tone(uint32_t frequency) {
-//    // Calculate half-period (in microseconds)
-//    uint32_t half_period_us = 500000 / frequency;
-//
-//    // Toggle buzzer pin
-//    while (1) {
-//        MAP_GPIO_setOutputHighOnPin(BUZZER_PIN);
-//        // Delay for half the period
-//        uint32_t delay_cycles = half_period_us * (SystemCoreClock / 1000000);
-//        uint32_t i = 0;
-//        for (i = 0; i < delay_cycles; i++) {
-//            __nop();
-//        }
-//        MAP_GPIO_setOutputLowOnPin(BUZZER_PIN);
-//        // Delay for the other half of the period
-//        for (i = 0; i < delay_cycles; i++) {
-//            __nop();
-//        }
-//    }
-//}
-//
-//// Function to stop tone generation
-//void noTone(void) {
-//    // Simply return from the tone function to stop generating tone
-//    return;
-//}
+
+void addChordNote(uint16_t frequency, uint8_t volume) {
+    if(notesPlayed < MAX_NOTES) {
+        currentFreq[notesPlayed] = frequency;
+        currentVolume[notesPlayed] = volume;
+        notesPlayed++;
+        playChord();
+    }
+    else {
+        printf("Cannot add more notes");
+    }
+}
+
+void removeChordNote(uint16_t frequency) {
+    for(i = 0; i < notesPlayed; i++) {
+        if(currentFreq[i] == frequency) {
+            uint16_t j = 0;
+            for(j = i; j < notesPlayed - 1; j++) {
+                currentFreq[j] = currentFreq[j + 1];
+                currentVolume[j] = currentVolume[j + 1];
+            }
+            notesPlayed--;
+            playChord();
+            break;
+        }
+    }
+    printf("Note not found");
+}
